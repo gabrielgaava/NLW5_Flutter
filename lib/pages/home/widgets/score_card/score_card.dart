ef49494ec544af7ed29ce189ae086021c5f1cacb
gabrielgaava <gabrielgava@estudioddr.dev>
2021-04-20 13:21:07 -0300

    Primeira aula concluida

---
+++ lib/pages/home/widgets/score_card/score_card.dart
@@ -0,0 +1,47 @@
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/pages/home/widgets/chart/chart.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15)
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: CircularChart()
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Vamos começar!", style: AppTextStyles.heading,),
+                      Text("Complete os desafios e avançe em conhecimento", style: AppTextStyles.body,),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
